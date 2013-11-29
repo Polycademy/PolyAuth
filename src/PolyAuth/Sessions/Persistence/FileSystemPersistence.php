88522b949a2485a8a0fa15880670a50edede2fd2
CMCDragonkai <cmcdragonkai@gmail.com>
2013-11-29 23:04:29 +1100

    filesystem persistence should contain its sessions in a subdirectory even when inside the session tmp directory to prevent conflicts

--- src/PolyAuth/Sessions/Persistence/FileSystemPersistence.php
+++ src/PolyAuth/Sessions/Persistence/FileSystemPersistence.php
@@ -8,14 +8,14 @@ use PolyAuth\Options;
 
 class FileSystemPersistence extends AbstractPersistence{
 
-	//needs to accept the encrypt mechanism! then it will encrypt information
 	public function __construct(FileSystem $driver = null, Pool $cache = null, Options $options = null){
 	
 		$options = ($options) ? $options : new Options;
 		if(!empty($options['session_save_path'])){
 			$driver = ($driver) ? $driver : new FileSystem(array('path' => $options['session_save_path']));
 		}else{
-			$driver = ($driver) ? $driver : new FileSystem(array('path' => session_save_path()));
+			//the reason we put it in its own directory is because some of the caching functions (such as purge) may conflict with files that are not part the stash library, such as for example session files that are placed there by native session drivers
+			$driver = ($driver) ? $driver : new FileSystem(array('path' => session_save_path() . '/polyauth'));
 		}
 		$cache = ($cache) ? $cache : new Pool;
 		$cache->setDriver($driver);
