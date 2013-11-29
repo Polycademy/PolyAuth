38f0edb13f1f07769edf03f3273777e28c5410d4
CMCDragonkai <cmcdragonkai@gmail.com>
2013-11-30 03:18:53 +1100

    fixed up UserAccount, and added comments for later work on the UserAccount

--- src/PolyAuth/UserAccount.php
+++ src/PolyAuth/UserAccount.php
@@ -6,12 +6,26 @@ use RBAC\Subject\Subject;
 use RBAC\Role\RoleSet;
 use RBAC\Role\Role;
 
+//THE KEY POINT IN KEEPING THE USERACCOUNT LEAN AND MEAN
+//IS THAT UPON THE CREATION OF USERACCOUNT
+//IT SHOULD ALL THE INFORMATION NECESSARY TO SPECIFY WHAT THIS USER IS, HAS, AND WHERE THE USER
+//AUTHORISED TO ANY PARTICULAR ACTION
+//IT DOES NOT NEED TO CONTACT THE DATABASE FOR ANYTHING, BECAUSE ALL THE INFORMATION IS ALREADY IN THIS ENTITY
+//THIS MEANS THAT IF THE USER IS UPDATED, THIS IS THE OBJECT YOU HAVE TO UPDATE, YOU PASS THIS INTO THE ACCOUNTSMANAGER
+//FURTHERMORE, WHEN SCOPES IMPLEMENTED, WE NEED TO LOAD THE SCOPES INTO THE USERACCOUNT AS WELL.
+//SO THE ACCOUNTS_MANAGER HAS TO GET ALL ASSOCIATED ACCESS TOKENS.
+//That could be access tokens that polyauth owns which means external api, it could also mean access tokens that this user owns in relation other users on this system, which also results in associated scopes.
+//
+//So when you're asking does this user have the permission to do something. This question is only in relation to the role this user possesses. It has nothing to do anyone else.
+//If you're asking does this person have the scope to do something. This question is really asking 2 things. Do you have a delegated resource, and do you have the empowered permission to do that action to that delegated resource. And a single user may have many delegated resources and different empowered permissions for each delegated resource. However each request to the system can only handle a single delegated resource and associated scope.
+
 /**
- * Will Contain
+ * Will Contain (this is todo, the authorized function doesn't yet check of scopes.. and needs to be streamlined anyway)
  *
- * ->api->github/facebook/..etc->request
- * ->api->polyauth->resource_owner
- * ->api->polyauth->access_token
+ * ->api->github/facebook/..etc->request (these external APIs would essentially be http request objects, like a thin wrapper over guzzle)
+ * ->api->polyauth->resource_owner (this is the id of resource owner, but the client is the user object)
+ * ->api->polyauth->access_token (this is the access token with regards to current system)
+ * The last 2 refer to the current system as an OAuth API
  *
  * ->authorized($permissions/$scope (in array), $roles, $user_id/$user_identity, $resource_owner_id){
  * }
@@ -19,13 +33,16 @@ use RBAC\Role\Role;
 
 class UserAccount extends Subject implements \ArrayAccess{
 
+	public $api;
+
 	protected $user_data = array();
-	protected $api; //should contain the object!
 
 	public function __construct($subject_id = false, RoleSet $role_set = null){
+
 		if($subject_id){
 			$this->set_user($subject_id, $role_set);
 		}
+
 	}
 	
 	/**
@@ -37,8 +54,112 @@ class UserAccount extends Subject implements \ArrayAccess{
 	 * @return null
 	 */	
 	public function set_user($subject_id, RoleSet $role_set = null){
+
 		parent::__construct($subject_id, $role_set);
 		$this->user_data['id'] = $subject_id;
+
+	}
+
+	/**
+	 * Checks if the user is logged in and possesses all the passed in parameters.
+	 * The parameters operate on all or nothing except $identities and $id. $identities and $id operates like "has to be at least one of them".
+	 * This first checks if the session exists, and if not checks if the user exists in this script's memory.
+	 * 
+	 * @param $permissions array of permission names | string | false
+	 * @param $roles array of role names | string | false
+	 * @param $identities array of user identities | string | false (this must match your login_identity option)
+	 * @param $ids array of user ids | integer | false
+	 * @return boolean
+	 */
+	public function authorized($permissions = false, $roles = false, $identities = false, $ids = false){
+			
+		$permissions = ($permissions) ? (array) $permissions : false;
+		$roles = ($roles) ? (array) $roles : false;
+		$identities = ($identities) ? (array) $identities : false;
+		$ids = ($ids) ? (array) $ids : false;
+
+		//anonymous users are not authorized
+		if($this['anonymous']){
+			return false;
+		}
+		
+		//id check
+		if($ids AND !in_array($this['id'], $ids)){
+			return false;
+		}
+
+		//identity check
+		if($identities AND !in_array($this[$this->options['login_identity']], $identities)){
+			return false;
+		}
+		
+		if($permissions){
+		
+			//check if the user has all the permissions
+			foreach($permissions as $permission_name){
+				if(!$this->has_permission($permission_name)){
+					return false;
+				}
+			}
+		
+		}
+		
+		if($roles){
+
+			foreach($roles as $role_name){
+				if(!$this->has_role($role_name)){
+					return false;
+				}
+			}
+		
+		}
+		
+		return true;
+	
+	}
+	
+	/**
+	 * Gets the user data array
+	 * 
+	 * @return array
+	 */
+	public function get_user_data(){
+
+		return $this->user_data;
+	
+	}
+
+	/**
+	 * Sets the user data for this user
+	 *
+	 * @param $data array
+	 * @return null
+	 */
+	public function set_user_data($data){
+	
+		$type = gettype($data);
+		
+		if($type != 'object' AND $type != 'array'){
+			return false;
+		}
+		
+		if($type == 'object'){
+			$data = get_object_vars($data);
+		}
+
+		//convert ipaddresses back to human readable form!
+		if(isset($data['ipAddress'])){
+			$data['ipAddress'] = inet_ntop($data['ipAddress']);
+		}
+		
+		$this->user_data = array_merge($this->user_data, $data);
+		
+	}
+
+	public function set_api($api){
+
+		$this->api = $api;
+
 	}
 	
 	/**
@@ -47,7 +168,9 @@ class UserAccount extends Subject implements \ArrayAccess{
 	 * @return object 
 	 */
 	public function get_role_set(){
+
 		return $this->getRoleSet();
+
 	}
 	
 	/**
@@ -56,7 +179,9 @@ class UserAccount extends Subject implements \ArrayAccess{
 	 * @return array of objects
 	 */
 	public function get_roles(){
+
 		return $this->getRoleset()->getRoles();
+
 	}
 	
 	/**
@@ -94,7 +219,9 @@ class UserAccount extends Subject implements \ArrayAccess{
 	 * @return array of objects
 	 */
 	public function get_permissions(){
+
 		return $this->getRoleset()->getPermissions();
+
 	}
 	
 	/**
@@ -105,132 +232,9 @@ class UserAccount extends Subject implements \ArrayAccess{
 	 * @return boolean
 	 */
 	public function has_permission($permission){
+
 		return $this->hasPermission($permission);
-	}
-	
-	/**
-	 * Sets the user data for this user
-	 *
-	 * @param $data array
-	 * @return null
-	 */
-	public function set_user_data($data){
-	
-		$type = gettype($data);
-		
-		if($type != 'object' AND $type != 'array'){
-			return false;
-		}
-		
-		if($type == 'object'){
-			$data = get_object_vars($data);
-		}
 
-		//convert ipaddresses back to human readable form!
-		if(isset($data['ipAddress'])){
-			$data['ipAddress'] = inet_ntop($data['ipAddress']);
-		}
-		
-		$this->user_data = array_merge($this->user_data, $data);
-		
-	}
-	
-	/**
-	 * Gets the user data array
-	 * 
-	 * @return array
-	 */
-	public function get_user_data(){
-		return $this->user_data;
-	}
-
-	//This authorized doesn't actually check whether this user is logged in or not. In fact the UserAccount is always available, but some UserAccounts are anonymous. So we do have to change the parameters of this function
-	/**
-	 * Checks if the user is logged in and possesses all the passed in parameters.
-	 * The parameters operate on all or nothing except $identities and $id. $identities and $id operates like "has to be at least one of them". But if identities and ids are both passed in, then one of them from each has to be true.
-	 * This first checks if the session exists, and if not checks if the user exists in this script's memory.
-	 * 
-	 * @param $permissions array of permission names | string | false
-	 * @param $roles array of role names | string | false
-	 * @param $identities array of user identities | string | false (this must match your login_identity option)
-	 * @param $ids array of user ids | integer | false
-	 * @return boolean
-	 */
-	public function authorized($permissions = false, $roles = false, $identities = false, $ids = false){
-	
-		$this->session_zone->start_session();
-		
-		$permissions = ($permissions) ? (array) $permissions : false;
-		$roles = ($roles) ? (array) $roles : false;
-		$identities = ($identities) ? (array) $identities : false;
-		$ids = ($ids) ? (array) $ids : false;
-
-		//if the session and $this->user don't exist, then the user is not logged in
-		//if one of them exists, then there's a chance that the session data got lost in a single instance of the script
-		//also compensates for if the user id is actually a zero
-		if(empty($this->session_zone['user_id']) AND $this->session_zone['user_id'] !== 0){
-			if(!$this->user instanceof UserAccount OR empty($this->user['id'])){
-				return false;
-			}else{
-				$user_id = $this->user['id'];
-			}
-		}else{
-			$user_id = $this->session_zone['user_id'];
-		}
-
-		//we finished reading from the session, commit it!
-		$this->session_zone->commit_session();
-		
-		//check if the user id actually exists (this may be redundant, but better safe than sorry)
-		$row = $this->storage->get_user($user_id);
-
-		//does the id exist?
-		if(!$row){
-			return false;
-		}
-		
-		//id check
-		if($ids AND !in_array($user_id, $ids)){
-			return false;
-		}
-
-		//identity check
-		if($identities AND !in_array($row->{$this->options['login_identity']}, $identities)){
-			return false;
-		}
-		
-		//reset the user variable if it does not exist, this is possible if the developer not use $this->start()
-		if(!$this->user instanceof UserAccount OR empty($this->user['id'])){
-			$this->user = $this->accounts_manager->get_user($user_id);
-		}
-		
-		if($permissions){
-		
-			//check if the user has all the permissions
-			foreach($permissions as $permission_name){
-				if(!$this->user->has_permission($permission_name)){
-					return false;
-				}
-			}
-		
-		}
-		
-		if($roles){
-		
-			//we need to acquire role objects first because has_role only accepts objects, not strings
-			$role_objects = $this->rbac->get_roles($roles);
-
-			foreach($role_objects as $role_object){
-
-				if(!$this->user->has_role($role_object)){
-					return false;
-				}
-			}
-		
-		}
-		
-		return true;
-	
 	}
 	
 	public function offsetSet($offset, $value) {
